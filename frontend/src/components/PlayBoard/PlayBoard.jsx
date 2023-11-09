83930279348dfc47ebebd3858d98510aef8a6c6a
Jyrko <uriydemoshenko@gmail.com>
2023-11-09 13:43:11 +0100

    Reset button. Decor elements

--- frontend/src/components/PlayBoard/PlayBoard.jsx
+++ frontend/src/components/PlayBoard/PlayBoard.jsx
@@ -7,7 +7,7 @@ const GRID_WIDTH = 6;
 
 function PlayBoard() {
     const [rows, setRows] = React.useState(generateEmptyCellValues()); // Find a more efficient way 
-    // const [key, setKey] = React.useState(0);
+    const [currentPlayer, setCurrentPlayer] = React.useState("X");
     let isCirclePlaying = false;
 
     function generateEmptyCellValues() {
@@ -19,19 +19,39 @@ function PlayBoard() {
     }
 
     function resetHandler(e) {
-        //Reset logic
         e.preventDefault();
+        setRows(generateEmptyCellValues());
+        document.querySelectorAll(".board-cell").forEach((cell) => {
+            cell.innerHTML = "";
+        });
     }
+
+    function isValidIndex(index) {
+        if (index < GRID_WIDTH) return true;
+
+        return rows[index - GRID_WIDTH] ? true : false;
+    }
+
     function cellHandler(e) {
         const newRows = rows;
         const index = e.target.getAttribute("data-index");
 
-        e.target.innerHTML = isCirclePlaying ? "O" : "X";
-        isCirclePlaying = !isCirclePlaying;
+        if (!e.target.innerHTML && isValidIndex(index)) {   
+            e.target.innerHTML = isCirclePlaying ? "O" : "X";
+            rows[index] = e.target.innerHTML;
 
-        rows[index] = isCirclePlaying ? "O" : "X";
-        console.log("Clicked " + index);
+            isCirclePlaying = !isCirclePlaying;
+        }
+        // setCurrentPlayer(isCirclePlaying ? "O" : "X"); //Causes the bug
+        console.log("Clicked: " + index);
         setRows(newRows);
+        console.log(rows);
+    }
+
+    function generateGrid() {
+        return rows.map((element, index, array) => {
+            return <div key={"cell-" + (array.length - index - 1)} data-index={array.length - index - 1} className='board-cell' onClick={cellHandler}>{element}</div>
+        });
     }
 
     React.useEffect(() => {
@@ -41,11 +61,15 @@ function PlayBoard() {
 
   return (
     <div className='playboard-component'>
+        <div className='time-elapsed-stat'>
+            <h3>Time elapsed: 00:00:00</h3>
+        </div>
+        <div>
+            <h3>Your turn: {currentPlayer}</h3>
+        </div>
         <div className='interface-wrapper'>
             <div className='board-field'>
-                {rows.map((element, index) => {
-                    return <div key={"cell-" + index} data-index={index} className='board-cell' onClick={cellHandler}>{element}</div>
-                })}
+                {generateGrid()}
             </div>
             <div className='board-control'>
                 <div className='buttons-wrapper'>
