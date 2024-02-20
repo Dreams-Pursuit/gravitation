20f0bad0e622906ce6598336a0fdb6b0ef04257f
masandrei <andreymarshin@gmail.com>
2024-02-20 16:18:17 +0100

    Validation of timer and styling

--- frontend/src/components/StartScreen/Setup.jsx
+++ frontend/src/components/StartScreen/Setup.jsx
@@ -1,4 +1,4 @@
-
+import "../../css/Setup.css"
 
 function SetUp({setGameStarted, setGameSetup}){
     function onStartOfflineModeButton(e) {
@@ -11,10 +11,22 @@ function SetUp({setGameStarted, setGameSetup}){
     }
 
     function onTimerCheck(e){
-        var timerCheckbox = document.getElementById("timer-check");
+        var timerCheckbox = document.getElementById("timer");
         var timerFields = document.getElementById("timer-value");
         console.log("Timer checked");
         timerFields.hidden = !timerCheckbox.checked;
+    }
+    function validateTimerInput(e){
+        if(e.target.value === ''){
+            console.log(e);
+            e.target.value = e.target.defaultValue;
+        }else if(Number.parseInt(e.target.value) < 0){
+            e.target.value = "00";
+        }else if(e.target.id == "timer-minute" && Number.parseInt(e.target.value) > 10){
+            e.target.value = "10";
+        }else if(e.target.id == "timer-second" && Number.parseInt(e.target.value) > 59){
+            e.target.value = "59";
+        }
     }
 
     return(
@@ -22,19 +34,26 @@ function SetUp({setGameStarted, setGameSetup}){
             <h1>
                 Settings
             </h1>
-            <form>
-                <input type="radio" id="classic" name="mode" checked />
-                <label for="classic">Classic Mode</label><br/>
-                <input type="radio" id="advanced" name="mode" />
-                <label for="advanced">Advanced mode</label><br/>
+            <form className="choose-mode">
+                <div className="mode-option">
+                    <input type="radio" id="classic" name="mode" checked />
+                    <label for="classic">Classic Mode</label><br/>
+                </div>
+                <div className="mode-option">
+                    <input type="radio" id="advanced"  name="mode" />
+                    <label for="advanced">Advanced mode</label><br/>
+                </div>
             </form>
-            <input type="text" id="player1Name" defaultValue="Player 1" pattern="[a-zA-Z0-9\s]+" required /><br/>
-            <input type="text" id="player2Name" defaultValue="Player 2" pattern="[a-zA-Z0-9\s]+" required /><br/>
-            <input type="checkbox" id="timer-check" onClick={onTimerCheck}/>
-            <label for="timer">Timer</label>
+            <input type="text" id="player1Name" defaultValue="Player 1" maxLength="25" required /><br/>
+            <input type="text" id="player2Name"  defaultValue="Player 2" maxLength="25" required /><br/>
+            <div id="timer-checkbox">
+                <input type="checkbox" id="timer" onClick={onTimerCheck}/>
+                <label for="timer">Timer</label>
+            </div>
             <div id="timer-value" hidden>
-                <input type="text" id="timer-minutes" defaultValue="00" pattern="[0-9]+"/>
-                <input type="text" id="timer-seconds" defaultValue="30" min="0" max="59" pattern="[0-9]+" required/>
+                <input type="number" id="timer-minute" defaultValue="00" min="0" max="10" size="2" onBlur={validateTimerInput}/>
+                <label>:</label>
+                <input type="number" id="timer-second" defaultValue="30" min ="0" max="59" size="2" onBlur={validateTimerInput} required/>
             </div>
             <button id="game-start-button" onClick={onStartOfflineModeButton}>Start game</button>
         </div>
