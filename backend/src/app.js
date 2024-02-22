093f655ae0992094cb1c771450762162202dac1b
Jyrko <uriydemoshenko@gmail.com>
2024-02-22 21:04:21 +0100

    stub for the player logic websockets. Added start script

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,12 @@
 const fastify = require("fastify");
 
 //Routes
-const game = require("../routes/game");
+const game = require("./routes/game");
 
 const build = (opts = {}) => {
   const app = fastify(opts);
+  app.register(require("@fastify/websocket"));
+
   app.register(game, { prefix: "/game" });
 
   return app;
--- backend/src/routes/game.js
+++ backend/src/routes/game.js
@@ -1,7 +1,41 @@
 "use strict";
 
 const game = async (fastify, _, done) => {
-  fastify.get("/", async (req, reply) => ({ hello: "world" }));
+
+  function broadcast(message) {
+    for (const client of fastify.websocketServer.clients) {
+      client.send(JSON.stringify(message));
+    }
+  }
+
+  const boards = new Map();
+  fastify.get("/play", { websocket: true }, (connection, req) => {
+    broadcast({ sender: "server", message: "new player connected" });
+    connection.socket.on("close", () => {
+      broadcast({ sender: "server", message: "player disconnected" });
+    });
+
+    connection.socket.on("message", (message) => {
+      const data = JSON.parse(message);
+      if (data.type === "join") {
+        if (boards.has(data.board)) {
+          boards.get(data.board).push(connection);
+        } else {
+          boards.set(data.board, [connection]);
+        }
+      }
+      if (data.type === "move") {
+        const board = boards.get(data.board);
+        if (board) {
+          for (const client of board) {
+            if (client !== connection) {
+              client.send(JSON.stringify(data));
+            }
+          }
+        }
+      }
+    });
+  });
 
   done();
 };
