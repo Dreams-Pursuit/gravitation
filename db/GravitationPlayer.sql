30bf3684c7c748c8f71397833459573f053c5285
masandrei <andreymarshin@gmail.com>
2024-02-25 22:32:15 +0100

    Added connection to PostgreSQL database

--- db/GravitationPlayer.sql
+++ db/GravitationPlayer.sql
@@ -1,8 +1,8 @@
 CREATE TABLE public.Players(
-	user_id SERIAL PRIMARY KEY,
+	user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	user_name VARCHAR(30) NOT NULL,
 	user_email VARCHAR(50) NOT NULL,
-	user_password VARCHAR(80) NOT NULL,
-	user_rating INT NOT NULL,
-	user_friends INT[]
+	user_password TEXT NOT NULL,
+	user_rating INT DEFAULT 1000,
+	user_friends INT[] DEFAULT {}
 )
